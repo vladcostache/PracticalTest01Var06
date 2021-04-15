68c1a23f5de994a81811d5bc117e205f0b87e7a4
Vlad Costache <vlad_ioan.costache@stud.acs.upb.ro>
2021-04-15 13:52:03 +0300

    D1

---
+++ PracticalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/ProcessingThread.java
@@ -0,0 +1,52 @@
+package ro.pub.cs.systems.eim.practicaltest01var06;
+
+import java.util.Date;
+import java.util.Random;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import ro.pub.cs.systems.eim.practicaltest01.general.Constants;
+
+public class ProcessingThread extends Thread {
+
+    private Context context = null;
+    private boolean isRunning = true;
+
+    private Random random = new Random();
+
+    private double arithmeticMean;
+    private double geometricMean;
+
+    public ProcessingThread(Context context, int firstNumber, int secondNumber) {
+        this.context = context;
+
+ 
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            sendMessage();
+            sleep();
+        }
+    }
+
+    private void sendMessage() {
+        Intent intent = new Intent();
+        context.sendBroadcast(intent);
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
+        }
+    }
+
+    public void stopThread() {
+        isRunning = false;
+    }
+}
