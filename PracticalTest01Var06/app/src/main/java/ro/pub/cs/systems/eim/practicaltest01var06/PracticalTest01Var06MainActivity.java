a604cabf58da5b00aa62fd81736abab81596585a
Vlad Costache <vlad_ioan.costache@stud.acs.upb.ro>
2021-04-15 12:14:08 +0300

    A1

---
+++ PracticalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest01var06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest01Var06MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var06_main);
+    }
+}
